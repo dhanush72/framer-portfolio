d8a85666d80f0a84bb910e1f7a8de885cdd4af3f
dhanush72 <dhanuraj7258@gmail.com>
2020-12-16 11:40:56 +0530

    added services page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AboutPage from "./pages/AboutPage";
+import ServcesPage from "./pages/ServicesPage";
 
 function App() {
   return (
     <div className="App">
       <AboutPage />
+      <ServcesPage />
     </div>
   );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,63 @@
+import React from "react";
+import collaboration from "../assets/images/collaboration.svg";
+import clock from "../assets/images/clock.svg";
+import diaphragm from "../assets/images/diaphragm.svg";
+import money from "../assets/images/money.svg";
+import teamwork from "../assets/images/teamwork.svg";
+
+const Services = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>High</h2> <span>quality</span> services
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="clock" />
+              <h3>Effecient</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed,
+              fugit?
+            </p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed,
+              fugit?
+            </p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="money" />
+              <h3>Money</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed,
+              fugit?
+            </p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="teamwork" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed,
+              fugit?
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={collaboration} alt="collaboration" />
+      </div>
+    </div>
+  );
+};
+
+export default Services;
