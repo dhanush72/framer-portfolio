d380bd606fdfedad6dd285d79c90ce521ce5761b
dhanush72 <dhanuraj7258@gmail.com>
2020-12-16 14:08:32 +0530

    added styles to faq page

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+import { AboutDiv } from "../styles";
 
 const Faq = () => {
   return (
-    <div className="faq">
+    <FaqDiv>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -15,6 +17,7 @@ const Faq = () => {
             adipisci dolorem eaque!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payment Methods</h4>
@@ -25,6 +28,7 @@ const Faq = () => {
             adipisci dolorem eaque!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What product do you?</h4>
@@ -35,9 +39,46 @@ const Faq = () => {
             adipisci dolorem eaque!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </FaqDiv>
   );
 };
+
+const FaqDiv = styled(AboutDiv)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h4 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+  }
+
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0;
+    width: 100%100%;
+  }
+
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 1rem 0;
+
+    p {
+      padding: 1rem 0;
+    }
+  }
+`;
 
 export default Faq;
